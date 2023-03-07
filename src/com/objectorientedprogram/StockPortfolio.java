b01ec4a2059f5bd171a813ceb5dda83680bb3b72
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-07 17:39:33 +0530

    Added: Welcome message

---
+++ src/com/objectorientedprogram/StockPortfolio.java
@@ -0,0 +1,7 @@
+package com.objectorientedprogram;
+
+public class StockPortfolio {
+    public static void main(String[] args) {
+        System.out.println("Welcome to stock account management");
+    }
+}
